a1d28dbe162d21a75c9dcff2371d8a26da1fcd58
ALLIAUME Hugo <kocal@live.fr>
2016-03-22 12:24:07 +0100

    EarthquakeActivity terminée imo

--- app/src/main/java/fr/kocal/android/iut_mini_projet/activities/EarthquakeActivity.java
+++ app/src/main/java/fr/kocal/android/iut_mini_projet/activities/EarthquakeActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,14 +13,19 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.text.DateFormat;
+import java.util.Date;
+
 import fr.kocal.android.iut_mini_projet.Earthquake;
 import fr.kocal.android.iut_mini_projet.R;
 
 public class EarthquakeActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
-
     Earthquake earthquake = null;
+    String sLocalisation, sMagnitude, sDate;
+
+    private TextView mLocalisation, mMagnitude, mDate;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +35,7 @@ public class EarthquakeActivity extends AppCompatActivity implements OnMapReadyC
         earthquake = (Earthquake) getIntent().getSerializableExtra("earthquake");
 
         initToolbar();
+        initValues();
         initMaps();
     }
 
@@ -50,15 +57,38 @@ public class EarthquakeActivity extends AppCompatActivity implements OnMapReadyC
         Double[] coordinates = earthquake.getCoordinates();
 
         LatLng place = new LatLng(coordinates[1], coordinates[0]);
-        mMap.addMarker(new MarkerOptions().position(place).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions().position(place).title("Tremblement de terre"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(place, mMap.getMinZoomLevel()));
-
 
         (new Handler()).postDelayed(new Runnable() {
             @Override
             public void run() {
-                mMap.animateCamera(CameraUpdateFactory.zoomTo(5));
+                mMap.animateCamera(CameraUpdateFactory.zoomTo(6));
             }
         }, 200);
+    }
+
+    private void initValues() {
+        mLocalisation = (TextView) findViewById(R.id.localisation);
+        mMagnitude = (TextView) findViewById(R.id.magnitude);
+        mDate = (TextView) findViewById(R.id.date);
+
+        // Formatage des coordonnées
+        Double[] coordinates = earthquake.getCoordinates();
+        sLocalisation = String.format(getString(R.string.coordinates),
+                coordinates[0], coordinates[1]);
+
+        // Formatage de la magnitude
+        sMagnitude = String.format(getString(R.string.magnitude), earthquake.getMagnitude());
+
+        // Formatage de la date
+        Date date = new Date(earthquake.getTime());
+        DateFormat dateFormat = DateFormat.getDateInstance();
+        sDate = dateFormat.format(date);
+
+        // Fill
+        mLocalisation.setText(sLocalisation);
+        mMagnitude.setText(sMagnitude);
+        mDate.setText(sDate);
     }
 }
